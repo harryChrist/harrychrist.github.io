00607ffab4e18a036b367bd0cb60bfbf2f173164
Harry <henriquecbgamer@gmail.com>
2025-03-07 23:22:05 -0300

    update metadados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,30 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Henrique Christ",
-  description: "Meu Portffólio - Henrique Christ",
+  description: "Meu Portfólio - Henrique Christ",
+  icons: {
+    icon: "/icon.png"
+  },
+  openGraph: {
+    title: "Henrique Christ",
+    description: "Meu Portfólio - Henrique Christ",
+    images: [
+      {
+        url: "/me.png", // Caminho para a imagem que você quer usar
+        width: 1200,
+        height: 630,
+        alt: "Henrique Christ - Portfólio",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Henrique Christ",
+    description: "Meu Portfólio - Henrique Christ",
+    images: ["/me.png"], // Caminho para a imagem que você quer usar
+  },
 };
+
 
 export default function RootLayout({
   children,
